bc8a4d262331226a0aeab9f4ab545d1f12df4a0f
Adinda Praditya <apraditya@gmail.com>
2016-09-23 18:09:56 +0800

    Move model on insights/index to the main insights route & Fix published-at insight.

--- app/pods/insights/route.js
+++ app/pods/insights/route.js
@@ -1,4 +1,11 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  model: function() {
+    return this.get('store').query('insight', {
+      'page[size]': 10,
+      'page[number]': 1,
+      include: 'content-verticals,primary-entity,account',
+    });
+  },
 });
