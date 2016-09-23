d8ef9c5e78438ab676ca09a3141d250d69a2934d
Adinda Praditya <apraditya@gmail.com>
2016-09-23 11:29:22 +0800

    Add Insight model

---
+++ app/models/insight.js
@@ -0,0 +1,13 @@
+import DS from 'ember-data';
+
+export default DS.Model.extend({
+  tagline: DS.attr('string'),
+  executiveSummary: DS.attr('string'),
+  imperative: DS.attr('string'),
+  slug: DS.attr('string'),
+  publishedAt: DS.attr('date'),
+  formatType: DS.attr('string'),
+  totalCombinedRead: DS.attr('number'),
+  countries: DS.attr(),
+  sectors: DS.attr()
+});
